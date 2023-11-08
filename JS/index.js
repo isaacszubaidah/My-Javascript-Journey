6383aeceb061b384abaeaaa4b957eb70b42b96a2
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-11-08 14:32:42 +0200

    add

---
+++ JS/index.js
@@ -0,0 +1,18 @@
+
+let language = "JavaScript";
+
+let score = 10;
+
+let length = 10;
+
+let width = 5;
+
+let areaRect = length*width;
+
+console.log(`The area of  rectangle is`.concat (areaRect));
+
+let num1 = 10;
+
+let num2 = 4;
+
+console.log(`${num1} / ${num2} = ${~~(num1/num2)} remainder ${num1%num2}`);
